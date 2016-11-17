8d25203cb0a20d73fd65da1e00e3d74c8079584f
dipdeb <netro.sai@gmail.com>
2016-11-17 15:49:05 +0530

    Add

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 	res.send("<html><body><h1>Hello World!</h1></body></html>");
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
